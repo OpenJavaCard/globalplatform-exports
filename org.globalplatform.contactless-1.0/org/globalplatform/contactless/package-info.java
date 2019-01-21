ebf29f841c0dfaf2d29cbaa461f43fdd8375c4f1
Ingo Albrecht <prom@berlin.ccc.de>
2019-01-21 08:53:27 +0100

    Unified gpapi-contactless-1.0
    Created a JAR with all the other files, copied the exp file to toplevel.
    
    This makes all the contactless APIs consistent.

--- org.globalplatform.contactless-1.0/org/globalplatform/contactless/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Provides a framework of classes and interfaces related to contactless
- * services defined for smart cards based on GlobalPlatform specifications.
- * @version This documentation describes API elements and behaviors associated
- * with version 1.0 of this package export file.
- */
-package org.globalplatform.contactless;
-
